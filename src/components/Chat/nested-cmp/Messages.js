0917aa98e637cb1d4f30c034cff7f9ffaf5234ab
student-PiyushMishra <student.piyushmishra@gmail.com>
2025-02-24 17:06:56 +0530

    refactored the messages div scroll-to-bottom

--- src/components/Chat/nested-cmp/Messages.js
+++ src/components/Chat/nested-cmp/Messages.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 
 import "./Messages.js.css";
 import Message from './Message';
 
-const Messages = ({messages, name}) => {
-    return (
-        <ScrollToBottom className="messages" followButtonClassName='custom-scroll-button'>
-            <div className="messagesContainer">
-                {
-                    messages.map((message, index) => {
-                        return (
-                            <Message key={index} message={message} name={name}/>
-                        )
-                    })
-                }
+const Messages = ({ messages, name }) => {
+    const messagesEndRef = useRef(null);
 
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+
+    return (
+        <ScrollToBottom mode='bottom' className="messages" followButtonClassName='custom-scroll-button'>
+            <div className="messagesContainer">
+                {messages.map((message, index) => (
+                    <Message key={index} message={message} name={name} />
+                ))}
+                <div ref={messagesEndRef} />
             </div>
         </ScrollToBottom>
-
-    )
+    );
 }
 
 export default Messages;
